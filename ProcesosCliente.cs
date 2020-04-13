c0d35a0f31323582d29270584979096801ddc195
RubenAlonzo <63566834+RubenAlonzo@users.noreply.github.com>
2020-04-13 02:51:57 +0200

    Add files via upload

---
+++ ProcesosCliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaDatos;
+
+
+namespace CapaNegocios
+{
+    //Procesos intermedio entre los formularios y la base de datos
+    public class ProcesosCliente
+    {
+        ConsultasClientes objConsulta = new ConsultasClientes();
+
+        public DataTable MostrarClientes()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objConsulta.Mostrar();
+
+            return tabla;
+        }
+        public void InsertarCliente(string nombre, string apellido, string cedula, string direccion, string edad, string telefono)
+        {
+            objConsulta.Insertar(nombre, apellido, cedula, direccion, Convert.ToByte(edad), telefono);
+        }
+        public void EditarCliente(string nombre, string apellido, string cedula, string direccion, string edad, string telefono, string id)
+        {
+            objConsulta.Editar(nombre, apellido, cedula, direccion, Convert.ToByte(edad), telefono, Convert.ToInt32(id));
+        }
+        public void EliminarCliente(string id)
+        {
+            objConsulta.Eliminar(Convert.ToInt32(id));
+        }
+        public DataTable BuscarCliente(string nombre)
+        {
+            DataTable tabla2 = new DataTable();
+            tabla2 = objConsulta.Buscar(nombre);
+
+            return tabla2;
+        }
+    }
+}
---
+++ ProcesosPrestamo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CapaNegocios
+{
+    public class ProcesosPrestamo
+    {
+        ConsultasPrestamo objConsulta = new ConsultasPrestamo();
+
+        #region Metodos para el formulario de prestamos
+        public DataTable MostrarPrestamos()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objConsulta.Mostrar();
+
+            return tabla;
+        }
+        public DataTable BuscarPrestamo(string nombre)
+        {
+            DataTable tabla2 = new DataTable();
+            tabla2 = objConsulta.Buscar(nombre);
+
+            return tabla2;
+        }
+        public void EliminarPrestamo(string num)
+        {
+            objConsulta.Eliminar(Convert.ToInt32(num));
+        }
+
+        // ****** Manejo de pagos *******
+
+        public void Pagar(string idPrestamo, double pago)
+        {
+            objConsulta.Pagar(Convert.ToInt32(idPrestamo), pago);
+        }
+        public DataTable BalancesData(string id)
+        {
+            DataTable tabla = new DataTable();
+            tabla = objConsulta.Balances(Convert.ToInt32(id));
+
+            return tabla;
+        }
+        public void ActualizarPrestamos(string id, string monto, string cuota, string modalidad, string interes, string fecha)
+        {
+            //Elimina el signo de porcentaje del interes
+            string[] numInteres = interes.Split('%');
+            objConsulta.ActualizarPrestamos(Convert.ToInt32(id), Convert.ToDouble(monto), Convert.ToInt32(cuota), modalidad, Convert.ToInt32(numInteres[0]), Convert.ToDateTime(fecha));
+        }
+        #endregion
+
+        #region Metodos usados en el formulario de Nuevo prestamo
+
+        public DataTable MostrarClientes()
+        {
+            DataTable clientes = objConsulta.MostrarClientes();
+            return clientes;
+        }
+        public string GetID(string nombre)
+        {
+            string ID = objConsulta.GetID(nombre);
+            return ID;
+        }
+        public void InsertarNuevoPrestamo(string id, string monto, string cuota, string interes, string modalidad, string fecha)
+        {
+            //Elimina en signo de % en el ComboBox de interes
+            string[] InteresValor = interes.Split('%');
+            objConsulta.Insertar(Convert.ToByte(id), Convert.ToDouble(monto), Convert.ToByte(cuota), Convert.ToByte(InteresValor[0]), modalidad, fecha);
+        }
+        #endregion
+
+    }
+}
